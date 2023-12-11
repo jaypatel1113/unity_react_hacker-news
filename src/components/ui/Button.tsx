0c7e70be45d239be450d2d4811ef9139a99cab84
Jay Patel <jay64441860@gmail.com>
2023-12-11 19:36:06 +0530

    updated font in button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Button: React.FC<Props> = ({fn, total, visible}): React.ReactNode => {
     return (
         <div
-            className="mt-5 bg-[#f1daf2] text-[#df6cdb] max-w-max px-4 py-1 rounded-full text-base font-semibold cursor-pointer"
+            className="mt-5 bg-[#f1daf2] text-[#df6cdb] max-w-max px-4 py-1 rounded-full text-base font-semibold cursor-pointer font-mono"
             onClick={fn}
         >
             Load more ({total - visible} remaining)
